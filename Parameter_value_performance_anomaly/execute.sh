892847544df91eda59aca90940e27f8e7e4cd636
wapiti08 <tzrzhuoran@163.com>
2019-10-10 13:17:45 +0100

    Update execute.sh

--- Parameter_value_performance_anomaly/execute.sh
+++ Parameter_value_performance_anomaly/execute.sh
@@ -7,4 +7,4 @@ echo the root_dir is default, you can modify it according to your situation
 path1 = '../Dataset/Linux/Client/Client_structured/log_value_vector.csv'
 path2 = '../Dataset/Linux/Client/Client_structured/key_num_para_dict.csv'
 path3 = '../Dataset/Linux/Client/Client_structured/Event_npy/'
-python3 Parameter_value_vector_anomaly_client.py --p1 $path1 --p2 $path2 --p3 $path3
+python3 Parameter_value_vector_anomaly.py --p1 $path1 --p2 $path2 --p3 $path3
