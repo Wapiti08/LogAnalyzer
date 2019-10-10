7f4192f3983c7a754f94c6509974378a61f9fd26
wapiti08 <tzrzhuoran@163.com>
2019-10-10 13:15:49 +0100

    Add files via upload
    update the prediction part, add the module of confidence intervial

--- Parameter_value_performance_anomaly/anomaly_predict.py
+++ Parameter_value_performance_anomaly/anomaly_predict.py
@@ -1,17 +1,41 @@
-import joblib
-import matplotlib.pyplot import plt
-from matrix_analyse_report_anomaly import *
+import numpy as np
+import scipy.stats as st
 
-model = joblib.load(model.pkl)
+def confidence_intervial(confidence, mses_list):
+    # define the intervial tuple
+    intervial = None
+    intervial = st.t.interval(confidence, len(mses_list)-1, loc=np.mean(mses_list), scale=st.sem(mses_list))
 
-yhat = model.predict(x_test)
+    return intervial
 
+def anomaly_report(mses_list,file_number):
+    # here we use the max value as the threshold
+    confidence_intervial_fp1 = confidence_intervial(0.98, mses_list)
+    # it is for the false positive detection
+    threshold1 = confidence_intervial_fp1[1]
+    confidence_intervial_fp2 = confidence_intervial(0.99, mses_list)
+    # it is for the false positive detection
+    threshold2 = confidence_intervial_fp2[1]
+    confidence_intervial_an = confidence_intervial(0.999, mses_list)
+    # it is for the anomaly detection
+    threshold3 = confidence_intervial_an[1]
+    # record the potential anomaly logs
+    suspicious_logs = []
+    # record the false positive logs
+    fp_logs = []
+    for i in range(len(mses_list)):
+        if mses_list[i] > threshold3:
+            print('The {}th log in matrix {} is suspiciously anomaly'.format(i, file_number[0]))
+            suspicious_logs.append(i)
+        elif mses_list[i] > threshold1:
+            print('The {}th log in matrix {} is false positive'.format(i, file_number[0]))
+            fp_logs.append(i)
+        else:
+            continue
+    return threshold1, threshold2, threshold3, suspicious_logs, fp_logs
 
-
-plt.plot(x_list, rmses)
-plt.ylabel("Errors Values")
-file_number = re.findall('\d+', file)
-print("the file_number is:", file_number)
-plt.title(file_number[0] + ' ' + 'Errors Distribution')
-# plt.title(file + ' ' + 'Errors Distribution')
-plt.show()
+if __name__ == '__main__':
+    mses_list = [1,2,3]
+    file_number = [23,0]
+    threshold1, threshold2, threshold3, suspicious_logs, fp_logs = anomaly_report(mses_list, file_number)
+    print(threshold1,threshold3,threshold2)
