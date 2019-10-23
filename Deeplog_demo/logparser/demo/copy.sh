247f74219719ab33d6815c784e5e6e5814a10aba
Wapiti08 <tzrzhuoran@gmail.com>
2019-10-23 13:14:53 +0100

    Deeplog_demo has been deleted

--- Deeplog_demo/logparser/demo/copy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo copying the structured csv to Dataset_ML to analyse the logs with Machine Learning Model
-
-cp ../../Dataset/Linux/Malicious_Separate_Structured_Logs/Integrated_structured_log.csv ../../Dataset_ML/Linux/Malicious/structured_log.csv
-
-cp ../../Dataset/Linux/Clear_Separate_Structured_Logs/Integrated_structured_log.csv ../../Dataset_ML/Linux/Clear/structured_log.csv
--- Deeplog_demo/logparser/demo/exec_com.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-path1="../../Dataset/Linux/Clear/"
-path2="../../Dataset/Linux/Clear_Separate_Structured_Logs/"
-
-python2 LenMa_demo.py --p1 $path1 --p2 $path2
-
