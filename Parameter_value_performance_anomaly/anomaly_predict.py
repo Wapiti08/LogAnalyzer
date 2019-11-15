4308903df7d4491d2d2d94c1d4150a6f86bab3cd
Wapiti08 <tzrzhuoran@163.com>
2019-11-15 15:28:33 +0000

    Update anomaly_predict.py

--- Parameter_value_performance_anomaly/anomaly_predict.py
+++ Parameter_value_performance_anomaly/anomaly_predict.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scipy.stats as st
 
-def confidence_intervial(confidence, mses_list):
+def confidence_interval(confidence, mses_list):
     # define the intervial tuple
     intervial = None
     intervial = st.t.interval(confidence, len(mses_list)-1, loc=np.mean(mses_list), scale=st.sem(mses_list))
@@ -10,15 +10,15 @@ def confidence_intervial(confidence, mses_list):
 
 def anomaly_report(mses_list,file_number):
     # here we use the max value as the threshold
-    confidence_intervial_fp1 = confidence_intervial(0.98, mses_list)
+    confidence_intervial_fp1 = confidence_interval(0.98, mses_list)
     # it is for the false positive detection
     threshold1 = confidence_intervial_fp1[1]
-    confidence_intervial_fp2 = confidence_intervial(0.99, mses_list)
+    confidence_intervial_fp2 = confidence_interval(0.99, mses_list)
     # it is for the false positive detection
     threshold2 = confidence_intervial_fp2[1]
-    confidence_intervial_an = confidence_intervial(0.999, mses_list)
+    confidence_intervial_an = confidence_interval(0.999, mses_list)
     # it is for the anomaly detection
-    threshold3 = confidence_intervial_an[1]
+    threshold3 = confidence_interval_an[1]
     # record the potential anomaly logs
     suspicious_logs = []
     # record the false positive logs
